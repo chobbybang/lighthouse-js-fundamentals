09399ca4edc6c1510eea7252f9a04d2b4a7e6d67
bobby <robert.c.90d@gmail.com>
2016-10-22 01:56:50 +0000

    added arrays ice cream

---
+++ arrays-ice-cream.js
@@ -0,0 +1,16 @@
+// Do not modify these first two lines
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Step 1 
+iceCreamFlavours[5] = "root beer";
+console.log(iceCreamFlavours);
+
+// Step 2
+console.log(iceCreamFlavours[0]);
+
+// Step 3
+console.log(iceCreamFlavours[5]);
+
+// Step 4
+console.log(iceCreamFlavours.length);
